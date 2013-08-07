dc84ccc661a0d3d106f12c825fae1fd668865569
Q. Parker <ghostdancex@gmail.com>
2013-08-07 15:57:43 -0500

    tentative rendering work

--- Makefile
+++ Makefile
@@ -5,8 +5,12 @@ CC=gcc
 CFLAGS=-g -std=gnu99 -Wall
 
 vfr: 
-	$(CC) -I. $(shell gdal-config --cflags) -I$(srcdir) \
-        $(CFLAGS) \
+	$(CC) -I. \
+        $(shell gdal-config --cflags) \
+        $(shell pkg-config --cflags cairo) \
+        -I$(srcdir) \
+    $(CFLAGS) \
         -o $(builddir)vfr \
          $(srcdir)vfr.c  \
-         $(shell gdal-config --libs)
+         $(shell pkg-config --libs cairo) \
+         $(shell gdal-config --libs) \
--- src/vfr.c
+++ src/vfr.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
 
 #include "ogr_api.h"
+#include "ogr_srs_api.h"
 #include "cpl_error.h"
+
+#include "cairo.h"
 
 #if defined(__linux__)
 #define VFRSYSNAME "Linux"
 #else
 #define VFRSYSNAME "Unknown"
 #endif
+
+
+typedef struct vfr_style_s {
+    uint64_t bgcolor;
+} vfr_style_t;
 
 const char *g_progname;
 
@@ -48,7 +57,93 @@ static void usage(void){
 }
 
 static int runrender(int argc, char **argv) {
-    printf("runrender goes here\n");
+    
+    if(argc < 3) {
+        usage();
+        return(1);
+    }
+
+    int iw, ih;
+    iw = 3600;
+    ih = 1800;
+    const char *outfilenm = "vfrout.png";
+    vfr_style_t defaultstyle = {0};
+
+    OGRDataSourceH src;
+    OGRSFDriverH drvr;
+    src = OGROpen(argv[2], FALSE, &drvr);
+    if(src == NULL) {
+        fprintf(stderr, "could not open %s: %s\n", argv[2], CPLGetLastErrorMsg());
+        return 1;
+    }
+   
+    int i, layercount = OGR_DS_GetLayerCount(src), lfcount;
+    long j;
+    OGREnvelope ext = {};
+    OGREnvelope allext = {};
+    OGRLayerH layer;
+    OGRFeatureH ftr;
+    for(i=0; i<layercount; i++) {
+        layer = OGR_DS_GetLayer(src, i);
+        OGR_L_GetExtent(layer, &ext, 0);
+        if(i == 0 || ext.MinX < allext.MinX) {
+            allext.MinX = ext.MinX;
+        }
+        if(i == 0 || ext.MinY < allext.MinY) {
+            allext.MinY = ext.MinY;
+        }
+        if(i == 0 || ext.MaxX > allext.MaxX) {
+            allext.MaxX = ext.MaxX;
+        }
+        if(i == 0 || ext.MaxY > allext.MaxY) {
+            allext.MaxY = ext.MaxY;
+        }
+    }
+    printf("\textent: %0.2f, %0.2f, %0.2f, %0.2f\n", 
+        allext.MinX, allext.MinY, allext.MaxX, allext.MaxY);
+    double pxw, pxh;
+    pxw = (ext.MaxX - ext.MinX)/iw;
+    pxh = (ext.MaxY - ext.MinY)/ih;
+    printf("pxw,pxh: %f, %f\n", pxw, pxh);
+    cairo_surface_t *surface;
+    cairo_t *cr;
+    surface = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, iw, ih);
+    cr = cairo_create(surface);
+    cairo_rectangle(cr, 0, 0, iw, ih);
+    cairo_set_source_rgb(cr, 255, 255, 255);
+    cairo_fill(cr);
+    char *json;
+    OGRGeometryH geom;
+    double x, y, z, pxx, pxy;
+    for(i=0; i<layercount; i++) {
+        layer = OGR_DS_GetLayer(src, i);
+        lfcount = OGR_L_GetFeatureCount(layer, 0);
+        for(j=0; j<lfcount; j++) {
+            ftr = OGR_L_GetFeature(layer, j);
+            geom = OGR_F_GetGeometryRef(ftr);
+            json = OGR_G_ExportToJson(geom);
+            printf("json: %s\n", json);
+            switch(OGR_G_GetGeometryType(geom)) {
+                case wkbPoint:
+                    OGR_G_GetPoint(geom, 0, &x, &y, &z);
+                    pxx = (x - allext.MinX)/pxw;
+                    pxy = ((allext.MaxY- y) - allext.MinY)/pxh;
+                    printf("%f, %f -> %f, %f\n", x, y, pxx, pxy);
+                    cairo_arc(cr, pxx, pxy, 4, 0, 2*M_PI);
+                    cairo_set_source_rgb(cr, 255, 0, 0);
+                    cairo_fill_preserve(cr);
+                    cairo_set_source_rgb(cr, 0, 0, 0);
+                    cairo_set_line_width(cr, 4);
+                    cairo_stroke(cr);
+                    break;
+                default:
+                    printf("unknown!\n");
+            }
+            OGRFree(json);
+            OGR_F_Destroy(ftr);
+        }
+    }
+    cairo_surface_write_to_png(surface, outfilenm);
     return 0;
 }
 
@@ -72,6 +167,9 @@ static int runinform(int argc, char **argv) {
     const char *lgeomtype = "UKNOWN";
     OGRLayerH layer;
     OGRFeatureH ftr;
+    char *srswkt;
+    OGRErr err;
+    OGREnvelope ext;
     for(i =0; i<srclcount; i++) {
         layer = OGR_DS_GetLayer(src, i);
         lfcount = OGR_L_GetFeatureCount(layer, TRUE);
@@ -81,6 +179,11 @@ static int runinform(int argc, char **argv) {
         }
         printf("\tlayer %d: %s [%s] - %d feature(s)\n", i, OGR_L_GetName(layer), 
             lgeomtype, lfcount);
+        OGR_L_GetExtent(layer, &ext, 0);
+        printf("\textent: %0.2f, %0.2f, %0.2f, %0.2f\n", 
+            ext.MinX, ext.MinY, ext.MaxX, ext.MaxY);
+        OSRExportToWkt(OGR_L_GetSpatialRef(layer), &srswkt);
+        printf("\t%s\n", srswkt);
     }
     OGR_DS_Destroy(src);
     return 0;
